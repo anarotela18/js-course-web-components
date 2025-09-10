1cce941ad4bd52ff9c5c330e6dbd5f72868a90f3
Ana Rotela <anarotela18@gmail.com>
2025-09-10 11:42:56 -0300

    Class Atribute added

--- atribute/my-element.js
+++ atribute/my-element.js
@@ -1,40 +1,42 @@
-class myElement extends HTMLElement(){
-    constructor(){
-        super();
-        this.attachShadow({ mode: `open`});
-
-        this.title     = this.getAttribute('title');
-        this.paragraph = this.getAttribute('paragraph');
-        this.imgSrc    = this.getAttribute('img');
-    };
-    getTemplate(){
-        const template = document.createElement("template");
-        template.innerHTML = `
-            <section>
-                <h2>${this.title}</h2>
-                <div>
-                    <p>${this.paragraph}</p>
-                    <img src="${this.imgSrc}"/>
-                </div>
-            </section>
-         ${this.getStyles()}
-        `;
-        return template;
-    }
-    getStyles(){
-        return `
-            <style>
-                h2{
-                    color: red;
-                }
-            </style>
-        `;
-    }
-    render(){
-        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
-    }
-    connectedCallback(){
-        this.render();
-    }
+class myElement extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.title     = this.getAttribute("title");
+    this.paragraph = this.getAttribute("paragraph");
+    this.img       = this.getAttribute("imge");
+  }
+  getTemplate() {
+    const template = document.createElement("template");
+    template.innerHTML = `
+      <section>
+        <h2>${this.title}</h2>
+        <div>
+          <p>${this.paragraph}</p>
+          <img src=${this.img}>
+        </div>
+      </section>
+      ${this.getStyles()}
+    `;
+    return template;
+  }
+  getStyles() {
+    return `
+      <style>
+        h2 {
+          color: orange;
+        }
+        p{
+          color: green;
+        }
+      </style>
+    `;
+  }
+  render() {
+    this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+  }
+  connectedCallback() {
+    this.render();
+  }
 }
 customElements.define("my-element", myElement);
