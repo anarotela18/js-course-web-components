b7144e2f9edc0f15bb356cc240de994d23b57963
Ana Rotela <anarotela18@gmail.com>
2025-09-12 15:28:59 -0300

    Class host

---
+++ host/my-element.js
@@ -0,0 +1,51 @@
+class myElement extends HTMLElement{
+    constructor(){
+        super();
+        this.attachShadow({mode:"open"});
+    }
+    getTemplate(){
+        const template = document.createElement("template");
+        template.innerHTML = `
+        <section>
+            <h1><slot name="title"></slot></h1>
+            <p><slot name="paragraph"></slot></p>
+            <slot></slot>
+        </section>
+        ${this.getStyles()}
+        `;
+        return template;
+    }
+    getStyles(){
+        return `
+            <style>
+                :host{
+                    display: inline-block;
+                    width: 100%;
+                    min-width: 300px;
+                    max-width: 450px;
+                    font-size: 20px;
+                    background: grey;
+                }
+                :host(.blue){
+                    background: blue;
+                }
+                :host([yellow]){
+                    background: yellow;
+                } 
+                :host([yellow]) h1{
+                    color: grey;
+                }
+                :host([yellow]) p{
+                    color: red;
+                }        
+                :host-context(article.card){
+                    display: block;
+                    max-width: 100%;
+                }       
+            </style>
+        `;
+    }
+    render(){
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+    }
+}
